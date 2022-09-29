955aa1fdc5b0e2a0bad59529dc3fe0af70780bf4
ZhouLiqian <96431886+ZhouLiqian@users.noreply.github.com>
2022-09-29 14:33:14 +0800

    加入EKF与IEKF滤波误差比较图
    IEKF效果优于EKF

--- IEKF_for_One_Div_Unline_System.m
+++ IEKF_for_One_Div_Unline_System.m
@@ -7,7 +7,7 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function IEKF_for_One_Div_Unline_System
 % 初始化
-T=50;
+T=100;
 Q=10;
 R=1;
 w=sqrt(Q)*randn(1,T);
@@ -21,34 +21,70 @@ for k=2:T
     x(k)=0.5*x(k-1)+2.5*x(k-1)/(1+x(k-1)^2)+8*cos(1.2*k)+w(k-1);% 真实状态值
     y(k)=x(k)^2/20+v(k);% 真实观测值
 end
-% IEKF滤波算法
-Xekf=zeros(1,T);
+
+% EKF滤波算法
+Xekf=zeros(1,T);% 后验估计值
 P0=eye(1);
 Xekf(1)=x(1);
-imax=50;% 最大迭代次数
+for k=2:T
+Xn=0.5*Xekf(k-1)+2.5*Xekf(k-1)/(1+Xekf(k-1)^2)+8*cos(1.2*k);% 状态预测
+Zn=Xn^2/20;% 观测预测
+F=0.5+2.5*(1-Xn^2)/(1+Xn^2)^2;% 状态转移矩阵
+H=Xn/10;% 观测矩阵
+P=F*P0*F'+Q;% 预测协方差矩阵
+K=P*H'*inv(H*P*H'+R);% 卡尔曼增益
+Xekf(k)=Xn+K*(y(k)-Zn);% 状态更新 Xn:先验估计值
+P0=(eye(1)-K*H)*P;% 协方差更新
+end
+% disp(Xekf);
+
+% IEKF滤波算法
+XIekf=zeros(1,T);
+PI0=eye(1);
+XIekf(1)=x(1);
+imax=100;% 最大迭代次数
 for k=2:T
 % 预测阶段
-Xn=0.5*Xekf(k-1)+2.5*Xekf(k-1)/(1+Xekf(k-1)^2)+8*cos(1.2*k);% 状态预测
-F=0.5+2.5*(1-Xn^2)/(1+Xn^2)^2;% 状态转移矩阵
-P=F*P0*F'+Q;% 预测协方差矩阵
+XIn=0.5*XIekf(k-1)+2.5*XIekf(k-1)/(1+XIekf(k-1)^2)+8*cos(1.2*k);% 状态预测
+FI=0.5+2.5*(1-XIn^2)/(1+XIn^2)^2;% 状态转移矩阵
+PI=FI*PI0*FI'+Q;% 预测协方差矩阵
 % 更新阶段
- X=Xn;
+ XI=XIn;
  for i=0:imax
-     H=X/10;% 雅可比矩阵
-     Zn=X^2/20;% 观测预测
-     K=P*H'*inv(H*P*H'+R);% 卡尔曼增益
-     X=Xn+K*(y(k)-Zn-H*(Xn-X));% 状态更新
+     HI=XI/10;% 雅可比矩阵
+     ZIn=XI^2/20;% 观测预测
+     KI=PI*HI'*inv(HI*PI*HI'+R);% 卡尔曼增益
+     XI=XIn+KI*(y(k)-ZIn-HI*(XIn-XI));% 状态更新
  end
- Xekf(k)=X;
- P0=(eye(1)-K*H)*P;% 协方差更新
+
+ % 保存最后一次迭代数据
+ XIekf(k)=XI;
+ PI0=(eye(1)-KI*HI)*PI;% 协方差更新
 end
+% disp(XIekf);
+
+% 求均方根误差
+RMSE_ekf = sqrt((Xekf - x).^2);
+RMSE_Iekf = sqrt((XIekf - x).^2);
+
 % 画图
-figure
-hold on;
-box on;
-plot(x,'-ko','Markerface','r');
-plot(Xekf,'-ks','Markerface','b');
+figure(1)
+plot(x,'r','linewidth',1);
+hold on
+plot(Xekf,'b','linewidth',1);
+hold on
+plot(XIekf,'k','linewidth',1);
 xlabel('时间/s','FontSize',10);
 ylabel('状态值x','FontSize',10);
-title('IEKF滤波结果','FontSize',16);
+title('滤波结果','FontSize',16);
+legend('真值','EKF滤波值','IEKF滤波值');
+
+figure(2)
+plot(RMSE_ekf,'b','linewidth',1);
+hold on
+plot(RMSE_Iekf,'k','linewidth',1);
+xlabel('时间/s','FontSize',10);
+ylabel('误差','FontSize',10);
+title('误差对比图','FontSize',16);
+legend('EKF与真值误差','IEKF与真值误差');
 
